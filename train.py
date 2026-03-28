9be96fd22d841701733240c8bcdd201a99355607
harsh25bai11182 <harsh.25bai11182@vitbhopal.ac.in>
2026-03-28 20:03:13 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,28 +3,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
 
-print("🔥 Training started...")
-
-# Load dataset
 data = pd.read_csv("data/feedback.csv", encoding="utf-8")
 
-print("Dataset loaded:")
-print(data.head())
-
-# Split data
 X = data['text']
 y = data['sentiment']
 
-# Convert text to numerical features
 vectorizer = CountVectorizer()
 X_vec = vectorizer.fit_transform(X)
 
-# Train model
 model = MultinomialNB()
 model.fit(X_vec, y)
 
-# Save model and vectorizer
 pickle.dump(model, open("model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
-
-print("✅ Model trained and saved successfully!")
